d19c4b35db4ea0b04ec3a5149ea132ff128d35f6
ryo-i <your.suzuyaka16@gmail.com>
2020-07-18 03:51:16 +0900

    ログインログアウト、アカウント新規作成後のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'top#home'
   devise_for :users
-  root 'posts#index'
   resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#deviceでのデフォルトはrootに遷移するので、ログイン後はindexに設定する必要がある
