eb00128c5f35c41446d7ef8b79caee49d149672d
ryo-i <your.suzuyaka16@gmail.com>
2020-07-18 19:58:25 +0900

    rubocopの導入及びrubocop.ymlの設定#17

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'top#home'
   devise_for :users
   devise_scope :user do
-    post 'users/guest_sign_in', to:'devise/sessions#new_guest'
+    post 'users/guest_sign_in', to: 'devise/sessions#new_guest'
   end
   resources :posts
 end
