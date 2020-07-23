463f649ba6cb4fd83b92dfd489709a470cea0fc0
ryo-i <your.suzuyaka16@gmail.com>
2020-07-23 23:45:16 +0900

    口コミ新規投稿(画像含む)及び投稿一覧の一部の基本機能を実装#9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @post = Post.all
+  end
 
-  def show; end
+  def show
 
-  def new; end
+  end
 
-  def edit; end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    #find_or_create_by を使ってみる
+    post = Post.new(post_params)
+    post.save!
+      #redirect_to post_url, notice: "口コミを投稿しました"
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image, :review, :review)
+  end
 end
