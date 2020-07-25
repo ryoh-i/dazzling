d84a3add70b982031a91277f78728ade93669ded
ryo-i <your.suzuyaka16@gmail.com>
2020-07-25 16:19:14 +0900

    ユーザー詳細ページのルーティングを設定#21

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'devise/sessions#new_guest'
+    get 'users/:id/show', to:'devise/registrations#show'
   end
   resources :posts
 end
