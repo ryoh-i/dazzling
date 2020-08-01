225bb55be2aa87191cd4e49dd47a31f9e2176b79
ryo-i <your.suzuyaka16@gmail.com>
2020-08-02 02:47:53 +0900

    PostモデルとUserモデルの関連付けを実施#51

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  belongs_to :user
   mount_uploader :image, ImageUploader
   validates :title, presence: true, length: { maximum: 15 }
   validates :content, presence: true, length: { maximum: 160 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :posts, dependent: :destroy
   mount_uploader :image, ImageUploader
 end
