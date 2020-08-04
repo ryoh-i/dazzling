bef5f563d5d509684830de2d22d3ed8af90afc57
ryo-i <your.suzuyaka16@gmail.com>
2020-08-04 22:17:10 +0900

    検出エラーの除外を実施#17

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class FavoritesController < ApplicationController
   # お気に入り
   # Favoriteモデルにuser_idとpost_idを格納
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Favorite < ApplicationRecord
   belongs_to :post
   belongs_to :user
