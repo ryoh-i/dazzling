3ea18c323751a15e3ca086fbf13255a5104b7305
ryo-i <your.suzuyaka16@gmail.com>
2020-08-07 22:59:49 +0900

    フォロー＆アンフォローをAjaxで実装の実施#57

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   # favorited_postsによってユーザーがどの投稿をお気に入りしているかを取得することができる
   has_many :favorited_posts, through: :favorites, source: :post
   # following_relationshipsモデルを架空で作成しており、follower_idを参考にfollowing_relationshipsモデルにアクセスするようにしている
-  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
   has_many :following, through: :following_relationships
-  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :follower_relationships
 
   # Include default devise modules. Others available are:
@@ -28,6 +28,7 @@ class User < ApplicationRecord
     following_relationships.find_by(following_id: user.id)
   end
 
+
   # フォローを実施する
   def follow(user)
     following_relationships.create!(following_id: user.id)
@@ -39,6 +40,6 @@ class User < ApplicationRecord
   end
 
   # Rspecテストのため、一時的に記載
-  # validates :name, presence: true
+  validates :name, presence: true
   validates :profile, length: { maximum: 200 }
 end
