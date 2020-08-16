bcc1ba823d150e6f28021638f3af1d489a76d21a
ryo-i <your.suzuyaka16@gmail.com>
2020-08-16 21:50:07 +0900

    検出エラーの除外を実施#17

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,7 @@
 class CommentsController < ApplicationController
-
   def create
     @post = Post.find(params[:post_id])
-    #buildを使い、@postのidをpost_idに含んだ形でcommentのインスタンスを作成
+    # buildを使い、@postのidをpost_idに含んだ形でcommentのインスタンスを作成
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     @comment.save
@@ -16,8 +15,8 @@ class CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id)
   end
-
 end
