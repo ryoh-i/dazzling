1ed2baeeacb85cdbfc360d6a918b7b45b9693ddd
ryo-i <your.suzuyaka16@gmail.com>
2020-08-22 11:00:00 +0900

    テストコミット

--- config/routes.rb
+++ config/routes.rb
@@ -20,14 +20,15 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :relationships, only: [:create, :destroy]
-  resources :messages, only: [:create]
-  resources :rooms, only: [:create,:show]
-
   resources :posts do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
+
+  resources :relationships, only: [:create, :destroy]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
+
 end
 
 # git stash save 7/19
