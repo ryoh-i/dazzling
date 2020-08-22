be3cc30dcd8c6ca801759383473c35afed67aeac
ryo-i <your.suzuyaka16@gmail.com>
2020-08-23 00:07:08 +0900

    お気に入り機能とダイレクト機能のエラー回避#51

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_142015) do
+ActiveRecord::Schema.define(version: 2020_08_22_144631) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_142015) do
     t.bigint "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
