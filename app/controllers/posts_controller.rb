497ec70798fe3604fc54cb79b41903e0206e3362
ryo-i <your.suzuyaka16@gmail.com>
2020-09-04 06:10:58 +0900

    コメントの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,11 @@
 class PostsController < ApplicationController
   # 投稿一覧のアクション
   def index
-    # 　@posts = Post.all.page(params[:page]).per(10)
+    # @posts = Post.all.page(params[:page]).per(10)
     # 上記では、検索機能で絞ることはできていないため、以下のようにpost.rbのsearchを使って絞る。
     @posts = Post.search(params[:search]).page(params[:page]).per(6)
 
+    # タグにて絞り込み
     if params[:tag_name]
       @posts = Post.tagged_with("#{params[:tag_name]}").page(params[:page]).per(6)
     end
