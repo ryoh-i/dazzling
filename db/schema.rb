0f69452c5804a9f9804778902754f8b7836ebf67
ryo-i <your.suzuyaka16@gmail.com>
2020-09-22 17:46:04 +0900

    ヘッダーボタン位置調整を実施#13

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_122644) do
+ActiveRecord::Schema.define(version: 2020_09_22_083334) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_122644) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.string "image"
     t.string "art_name"
     t.string "review"
